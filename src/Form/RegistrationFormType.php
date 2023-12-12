3ca40ff57d0b71f330b71b9bc30dbeb07c18120a
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-12 12:33:14 +0100

    added placeholders in register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,15 +20,33 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('first_name')
-            ->add('last_name')
-            ->add('country')
-            ->add('city')
-            ->add('street')
-            ->add('house')
-            ->add('zip_code')
-            ->add('phone')
+            ->add('email', null, [
+                'attr' => ['placeholder' => 'Please enter e-mail']
+            ])
+            ->add('first_name', null, [
+                'attr' => ['placeholder' => 'Please enter first name']
+            ])
+            ->add('last_name', null, [
+                'attr' => ['placeholder' => 'Please enter last name']
+            ])
+            ->add('country', null, [
+                'attr' => ['placeholder' => 'Please enter your country']
+            ])
+            ->add('city', null, [
+                'attr' => ['placeholder' => 'Please enter your city']
+            ])
+            ->add('street', null, [
+                'attr' => ['placeholder' => 'Please enter your street']
+            ])
+            ->add('house', null, [
+                'attr' => ['placeholder' => 'Please enter your house number']
+            ])
+            ->add('zip_code', null, [
+                'attr' => ['placeholder' => 'Please enter the zip code']
+            ])
+            ->add('phone', null, [
+                'attr' => ['placeholder' => 'Please enter your phone number']
+            ])
             ->add('picture', FileType::class, [
                 'attr' => ['class' => 'form-control', 'placeholder' => 'Please enter price'],
 
@@ -54,10 +72,8 @@ class RegistrationFormType extends AbstractType
             ->add('birth_date')
             
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Please enter a password'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -65,7 +81,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
