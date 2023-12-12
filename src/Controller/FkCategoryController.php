23cc62050409c58717bef3706cee3b5b34a0fdf0
alvarosrg <alvarosr@gmail.com>
2023-12-12 14:09:38 +0100

    crud (category), fileuploader

--- src/Controller/FkCategoryController.php
+++ src/Controller/FkCategoryController.php
@@ -11,7 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/fk/category')]
+use App\Service\FileUploader;
+
+#[Route('/category')]
 class FkCategoryController extends AbstractController
 {
     #[Route('/', name: 'app_fk_category_index', methods: ['GET'])]
@@ -23,13 +25,21 @@ class FkCategoryController extends AbstractController
     }
 
     #[Route('/new', name: 'app_fk_category_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $fkCategory = new FkCategory();
         $form = $this->createForm(FkCategoryType::class, $fkCategory);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $picture = $form->get('picture')->getData();
+            if ($picture) {
+                $pictureFileName = $fileUploader->upload($picture);
+            }else{
+                $pictureFileName = "default_category.jpg";
+            }
+            $fkCategory->setPicture($pictureFileName);
+
             $entityManager->persist($fkCategory);
             $entityManager->flush();
 
@@ -51,12 +61,24 @@ class FkCategoryController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_fk_category_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, FkCategory $fkCategory, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, FkCategory $fkCategory, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $form = $this->createForm(FkCategoryType::class, $fkCategory);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $picture = $form->get('picture')->getData(); // this is from the form (new picture)
+            if ($picture) {
+                if ($fkCategory->getPicture() != "default_category.jpg"){
+                    unlink($this->getParameter("picture_directory") . "/". $fkCategory->getPicture()); // from product old picture
+                }
+
+                $pictureFileName = $fileUploader->upload($picture);
+                $fkCategory->setPicture($pictureFileName);
+            }
+
+
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_fk_category_index', [], Response::HTTP_SEE_OTHER);
@@ -71,7 +93,11 @@ class FkCategoryController extends AbstractController
     #[Route('/{id}', name: 'app_fk_category_delete', methods: ['POST'])]
     public function delete(Request $request, FkCategory $fkCategory, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$fkCategory->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $fkCategory->getId(), $request->request->get('_token'))) {
+            if ($fkCategory->getPicture() != "default_category.jpg"){
+                unlink($this->getParameter("picture_directory") . "/". $fkCategory->getPicture()); // from product old picture
+            }
+
             $entityManager->remove($fkCategory);
             $entityManager->flush();
         }
--- src/Form/FkCategoryType.php
+++ src/Form/FkCategoryType.php
@@ -7,13 +7,52 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+
 class FkCategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('picture')
+        ->add('name', null, 
+        [
+            'constraints' => [
+                new NotBlank(['message' => "You cannot leave the input empty."]),
+                new Length(['min' => 3, 'minMessage' => "It must be 3 letters at least."]),
+            ],
+            'attr' => ['placeholder' => 'Please write a name.'],
+        ])
+        
+
+        ->add('picture', FileType::class, [
+            'attr' => ['class' => 'form-control', 'accept' => 'image/png , image/jpg, image/jpeg'],
+            'label' => 'Category image',
+
+            // unmapped means that this field is not associated to any entity property
+            'mapped' => false,
+
+            // make it optional so you don't have to re-upload the PDF file
+            // every time you edit the Product details
+            'required' => false,
+
+            // unmapped fields can't define their validation using annotations
+            // in the associated entity, so you can use the PHP constraint classes
+            'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'image/png',
+                        'image/jpg',
+                        'image/jpeg',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid image document'
+                ])
+            ],
+        ])
         ;
     }
 
