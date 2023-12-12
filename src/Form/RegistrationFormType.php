7b7ccacc94b6c8bdcb400bb63956a8c2d5df9c9f
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-12 11:52:52 +0100

    registration form order

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -52,14 +52,7 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('created_at')
             ->add('birth_date')
-            ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            
             ->add('plainPassword', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -77,6 +70,14 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+])
         ;
     }
 
