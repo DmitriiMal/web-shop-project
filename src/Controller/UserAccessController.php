53980496c391dbaee14c65244ab3034a11f5182c
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-13 14:10:25 +0100

    user routing

--- src/Controller/UserAccessController.php
+++ src/Controller/UserAccessController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
 
-#[Route('/user')]
 class UserAccessController extends AbstractController
 {
     #[Route('/', name: 'app_user', methods: ['GET'])]
