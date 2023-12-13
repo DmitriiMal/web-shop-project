6157b5851e503ea2030ef5cd83b85d9cbe97fae2
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-13 14:31:08 +0100

    user product details routing

--- src/Controller/UserAccessController.php
+++ src/Controller/UserAccessController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
+
 
 class UserAccessController extends AbstractController
 {
@@ -14,6 +16,14 @@ class UserAccessController extends AbstractController
     {
         return $this->render('user_access/index.html.twig', [
             'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
         ]);
     }
 }
