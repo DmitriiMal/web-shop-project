66e825abc0d9c42c59f48ce32d78c1cdf56607de
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-13 12:15:40 +0100

    product cartfk changed

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/static')]
 class StaticController extends AbstractController
 {
     #[Route('/', name: 'app_static')]
