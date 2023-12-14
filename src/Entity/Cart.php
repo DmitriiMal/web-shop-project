dbf29f4c7859a40793067ce12573b3b736e1aed3
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-14 15:58:03 +0100

    cart complete

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -17,7 +17,7 @@ class Cart
     private ?int $id = null;
 
     #[ORM\OneToOne(cascade: ['persist', 'remove'])]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\JoinColumn(nullable: true)]
     private ?User $fk_userID = null;
 
 
