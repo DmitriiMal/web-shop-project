6b3a7efa7a29320d3be294c5344156630204aa4d
tomxi <tom.wang@stud.fh-campuswien.ac.at>
2023-12-14 11:11:48 +0100

    product cart relation fixed

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'app_cart')]
-    public function index(): Response
+    #[Route('/cart/{o
+        ', name: 'app_cart')]
+    public function index(EntityManagerInterface $entityManager): Response
     {
         return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+            
         ]);
     }
 }
