05a62861c23bcb67c0ed71949c6f2200da5012b5
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-12-14 10:18:52 +0100

    added sweet alert

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,20 @@
+// ///////////////////////// //
+// ////// Sweet Alert ////// //
+// ///////////////////////// //
 
+let okAlert = () => {
+  Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your work has been saved',
+    showConfirmButton: false,
+    timer: 1500,
+  });
+};
+
+let button = document.getElementById('button');
+button.addEventListener('click', okAlert);
+
+// ///////////////////////// //
+// ///////// Swiper //////// //
+// ///////////////////////// //
