f6fdfc34f6b7f7fd83dd8c0b048280f9570c38f1
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-12-15 01:16:44 +0100

    edited swiper

--- public/js/script.js
+++ public/js/script.js
@@ -6,37 +6,8 @@ let okAlert = () => {
   Swal.fire({
     position: 'top-end',
     icon: 'success',
-    title: 'Your work has been saved',
+    title: 'Product added to cart',
     showConfirmButton: false,
     timer: 1500,
   });
 };
-
-let button = document.getElementById('button');
-button.addEventListener('click', okAlert);
-
-// ///////////////////////// //
-// ///////// Swiper //////// //
-// ///////////////////////// //
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-});
