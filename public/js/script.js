2ae5f20ad962dc81ae055aec0a27f76e4b496b66
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-12-18 15:26:34 +0100

    delete review button

--- public/js/script.js
+++ public/js/script.js
@@ -15,3 +15,19 @@ let okAlert = () => {
 // Forgotten password?
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+
+// ////////////////////////// //
+// ////// Cart buttons ////// //
+// ////////////////////////// //
+
+let plus = document.getElementsByClassName('plus');
+let minus = document.getElementsByClassName('minus');
+let quantity = document.getElementsByClassName('quantity').value;
+let order = document.getElementsByClassName('order');
+
+// for (let i = 0; i < plus.length; i++) {
+//   const element = plus[i].addEventListener('click', increase);
+// }
+
+// let increase = () => {};
+// console.log(typeof quantity);
